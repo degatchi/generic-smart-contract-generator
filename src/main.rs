751367fd983364ef9962736532668d03112101ff
DeGatchi <degatchi@protonmail.com>
2024-12-15 21:20:21 +1100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use std::{default, fmt::format};
-
 use hex::{decode, encode};
 
-fn main() {
-    println!("Generating Exploit, anon-kun.\nPlease, be patient.");
-    println!("Thanks for choosing ");
-}
+fn main() {}
 
+// Implement this to plug into `Contract::build` to generate the contracts
 pub enum Cmds {
     ApproveErc20,
     TransferErc20,
@@ -21,6 +18,7 @@ struct Contract {
     source: String,
 }
 impl Contract {
+		// Add more as you please/need
     pub const P1_OP: &'static str = "60";
     pub const P20_OP: &'static str = "73";
     pub const GAS_OP: &'static str = "5A";
@@ -47,7 +45,7 @@ impl Contract {
         contract
     }
 
-    // returns old and new
+    // Updates the offset with new nibbles (nibble = 1/2 byte)
     fn update_offset(&mut self, nibbles: usize) -> (usize, usize) {
         let old = self.offset_count;
         self.offset_count += nibbles;
@@ -55,14 +53,14 @@ impl Contract {
         println!("\n[Offset] Updated from {} to {}", old, new);
         (old, new)
     }
-
+	
+		// Returns the hexified offset from our usize counter
     pub fn offset(&self) -> String {
-        let hex = format!("{:02x}", self.offset_count);
-        // println!("count : {}", self.offset_count);
-        // println!("hex : {}\n", hex);
-        hex
+        format!("{:02x}", self.offset_count)
     }
 
+		// Adds onto the end of our existing calldata with new PACKED inputs
+		// Meaning they aren't left side padded to save gas and be more compact
     pub fn extend_calldata(&mut self, new_calldata: Vec<&str>) {
         println!("\n[Extending Calldata: {}]", new_calldata.len());
         println!("- [00] Old: {}", &self.calldata);
@@ -83,17 +81,25 @@ impl Contract {
         }
     }
 
+		// Convert our packed calldata into left side padded uint
+		// This is what functions normally take:
+		// 0x000000000000000000000000000000000000000000000000000000AABBCCDD
+		// 
+		// instead of right padded
+		// 0xAABBCCDD000000000000000000000000000000000000000000000000000000
+		//
+		// But signatures for protocols always 4 bytes long at the start 
+		// of the calldata
     pub fn pad_cd_to_mem(&self, cd_from: usize, cd_to: usize) -> String {
         println!("\n---\n");
         println!("[Unpack Calldata]");
 
         let mut seq = String::new();
-        let mut mem_offset: usize = 4;
-
-        let sig_offset = self.calldata_offsets[cd_from];
-
+        let mut mem_offset: usize = 4; // start from 4 bc we'll skip the sig
+				
         // Load and store sig far left
-        // 0xAABBCCDD 000000000000000000000000000000000000000000000000000000
+        // 0x AABBCCDD 000000000000000000000000000000000000000000000000000000
+        let sig_offset = self.calldata_offsets[cd_from];
         seq.extend([format!(
             "{}{:02x}{}{}00{}",
             Self::P1_OP,
@@ -102,7 +108,13 @@ impl Contract {
             Self::P1_OP,
             Self::MSTORE_OP,
         )]);
-
+				
+				// Skip the first offset because that'll be our signature
+				// and the following offsets are the variables we use for said
+				// function call
+				//
+				// You can extend this to be more optimised, of course, but it
+				// doesn't serve too much purpose aside from cost of tx
         for (o, offset) in self.calldata_offsets[cd_from..cd_to]
             .iter()
             .enumerate()
@@ -113,7 +125,9 @@ impl Contract {
             let mut word = self.calldata.split_at(*offset * 2).1;
             word = word.split_at((next_offset - offset) * 2).0;
             let padded = format!("{:0>64}", word);
-
+					
+						// how much we'll shift our rigt padded word to be left padded
+						// and therefore function calldata compatable
             let shr_amt = 64 - word.len();
 
             let calldata_load = format!("{:02x}{}", offset, Self::CD_LOAD_OP);
@@ -124,9 +138,10 @@ impl Contract {
             println!("[SHR 0x{:02x} Word {}]", shr_amt, word);
             println!("- [Calldata To MSTORE: {}] {}", o, sub_seq);
             println!("- [x] Padded Word {}\n", padded);
-
+						
+						// move onto the next word 
             mem_offset += 32;
-
+						
             seq.extend([sub_seq]);
         }
 
@@ -134,20 +149,36 @@ impl Contract {
         println!("\n---");
         seq
     }
-
+	
+		
+		// Modularised approve token component. Whenever you want an approve 
+		// call this helper function to generate the CALL for an ERC20 approve
+		// 
+		// You could definitely make a single generalised function and have 
+		// your monitoring system spit our the variables instead of hardcoding
+		// like i did here
     pub fn approve_token(&mut self, token: &str, to: &str, amount: &str) {
+    
+		    // Get our start offset so we can calculate our last one after
+		    // adding to the calldata and calldata_offsets
         let latest_offset = if let Some(x) = self.calldata_offsets.last() {
             *x
         } else {
             0
         };
-
+	
+				// Get our CALL memory offset before we update it 
+				let call_mem_offset = self.offset();
+			
         // Each nibble = 1 character (pretty convenient right?!)
         let calldata_len: usize = (Self::APPROVE_SIG.len() + to.len() + amount.len()) / 2;
         let _ = self.update_offset(calldata_len);
         self.extend_calldata(vec![Self::APPROVE_SIG, to, amount]);
+        
+        // Calldata To Memory before CALL (which it references)
         let cd_to_mem = self.pad_cd_to_mem(latest_offset, latest_offset + 3);
-
+				
+				// CALL arguments
         let calldata_size: String = format!("{:02x}", calldata_len);
         let ret: String = format!("{}00{}00", Self::P1_OP, Self::P1_OP);
         let arg: String = format!(
@@ -155,7 +186,7 @@ impl Contract {
             Self::P1_OP,
             calldata_size,
             Self::P1_OP,
-            self.offset()
+            call_mem_offset // get the latest offset before we update everything
         );
         let token: String = format!("{}{}", Self::P20_OP, token); // assuming isnt pass in as '0x...'
         let gas_call_pop: String = format!("{}{}{}", Self::GAS_OP, Self::CALL_OP, Self::POP_OP);
